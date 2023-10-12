7111105e12a077b37b4d3dfab60c7d0da65c8659
Mirza-Hassan <hassanbarlas50@gmail.com>
2023-10-12 08:42:00 +0200

    Update and rename de.js to de.ts

--- Languages/de.js
+++
@@ -1,6 +0,0 @@
-export default {
-    skip: 'Überspringen',
-    notification: 'Benachrichtigungen zulassen',
-    notification_message: 'Erhalten Sie Benachrichtigungen, um Ihre Motivation zu steigern',
-  };
-  
---
+++ Languages/de.ts
@@ -0,0 +1,5 @@
+export default {
+  skip: 'Überspringen',
+  notification: 'Benachrichtigungen zulassen',
+  notification_message: `Erhalten Sie Benachrichtigungen${"\n"}, um Ihre Motivation zu steigern`,
+};
