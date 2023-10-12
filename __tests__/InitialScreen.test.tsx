ede8d573a655ab9f834f161accdd6bde143a2a51
Mirza-Hassan <hassanbarlas50@gmail.com>
2023-10-12 07:08:24 +0200

    Update and rename InitialScreen.test.js to InitialScreen.test.tsx

--- __tests__/InitialScreen.test.tsx
+++ __tests__/InitialScreen.test.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, RenderAPI } from '@testing-library/react-native';
 import InitialScreen from '../component/InitialScreen';
 
-const navigation = {
+interface Navigation {
+  navigate: jest.Mock;
+}
+
+const navigation: Navigation = {
   navigate: jest.fn(),
 };
 
 describe('InitialScreen', () => {
   it('renders correctly', () => {
-    const { getByText } = render(<InitialScreen navigation={navigation} />);
+    // @ts-ignore
+    const { getByText }: RenderAPI = render(<InitialScreen navigation={navigation} />);
     expect(getByText("For a fit and relaxed pregnancy")).toBeTruthy();
     expect(getByText("Get Started")).toBeTruthy();
     expect(getByText("Or Login")).toBeTruthy();
