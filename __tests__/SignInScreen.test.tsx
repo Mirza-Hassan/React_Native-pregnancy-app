de7166d1335ff582b87954db7818738a65d27c44
Mirza-Hassan <hassanbarlas50@gmail.com>
2023-10-12 07:09:03 +0200

    Update and rename SignInScreen.test.js to SignInScreen.test.tsx

--- __tests__/SignInScreen.test.tsx
+++ __tests__/SignInScreen.test.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
 import SignInScreen from '../component/SignInScreen';
 
+interface Navigation {
+  navigate: jest.Mock;
+}
+
+const navigation: Navigation = {
+  navigate: jest.fn(),
+};
+
 describe('SignInScreen', () => {
   it('disables the "Log in" button when no input is provided', () => {
-    const { getByTestId, queryByTestId } = render(<SignInScreen />);
+    // @ts-ignore
+    const { getByTestId, queryByTestId } = render(<SignInScreen navigation={navigation} />);
     const loginButton = getByTestId('login-button');
+    // @ts-ignore
     expect(loginButton).toBeDisabled();
     fireEvent.changeText(getByTestId('email-input'), 'test@example.com');
     fireEvent.changeText(getByTestId('password-input'), 'password123');
+    // @ts-ignore
     expect(loginButton).toBeEnabled();
   });
 });
